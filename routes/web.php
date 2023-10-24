7e2fdcaa335ac9e4d427d21e65e5bc8b76758221
saidilhalim <saidilhalim@mail.ugm.ac.id>
2023-10-24 12:28:54 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterController;
+
+
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('/dashboard', [LoginRegisterController::class, 'dashboard'])->name('dashboard');
+    Route::post('/logout', [LoginRegisterController::class, 'logout'])->name('logout');
+});
+
+Route::controller(LoginRegisterController::class)->group(function () {
+    Route::get('/register', [LoginRegisterController::class, 'register'])->name('register');
+    Route::post('/store', [LoginRegisterController::class, 'store'])->name('store');
+    Route::get('/login', [LoginRegisterController::class, 'login'])->name('login');
+    Route::post('/authenticate', [LoginRegisterController::class, 'authenticate'])->name('authenticate');
+});
